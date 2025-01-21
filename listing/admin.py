31a6173a15393cafdf998127989424cddcbc0e06
Mark kibocha <mark.varu@student.cuk.ac.ke>
2025-01-21 11:59:43 +0300

    Update on admin.py

--- listing/admin.py
+++ listing/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
-from listing.models import Property, PropertyType, Unit, SpaceType, UnitImage, PropertyAmenity, TestGis
+from listing.models import Property, PropertyType, PropertyFeature, Unit, SpaceType, UnitImage, PropertyAmenity, TestGis
 
 # Register your models here.
 admin.site.register(Property)
 admin.site.register(SpaceType)
 admin.site.register(UnitImage)
 admin.site.register(Unit)
-admin.site.register(PropertyType)
+admin.site.register(PropertyType),
+admin.site.register(PropertyFeature)
 admin.site.register(PropertyAmenity)
 admin.site.register(TestGis)
