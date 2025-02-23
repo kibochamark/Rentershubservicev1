2f19da98fdb7c1bc46793714fefcf88da79a434b
kibochamark <mark.varu@student.cuk.ac.ke>
2025-02-23 15:56:30 +0300

    added filter by approval status on properties

--- listing/filterset.py
+++ listing/filterset.py
@@ -7,7 +7,8 @@ class PropertyFilter(django_filters.FilterSet):
     rent_price=django_filters.RangeFilter(field_name="rent_price")
     propertytype_name=django_filters.CharFilter(field_name="property_type__name", lookup_expr="icontains")
     managed_by=django_filters.CharFilter(field_name="managed_by", lookup_expr="icontains")
+    is_approved=django_filters.BooleanFilter(field_name="is_approved")
 
     class Meta:
         model = Property
-        fields =['rent_price', 'propertytype_name', 'managed_by']
+        fields =['rent_price', 'propertytype_name', 'managed_by', 'is_approved']
